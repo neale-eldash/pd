567d1cdd32c4d3f1eb1aad316e5f8a3310ef28ef
NEALE AHMED EL-DASH <neale.eldash@gmail.com>
2019-04-11 15:25:21 -0300

    adding xlsx_table function and help

--- R/export_table_xlsx.R
+++ R/export_table_xlsx.R
@@ -1,113 +1,114 @@
-# require(dplyr)
-# require(tidyr)
-# library(tableHTML)
-# require(xlsx)
-#
-# dir <- "E:\\DADOS\\CONSULTORIA\\PESSOAS JURÍDICAS\\FlyFrog\\Perception - 10-04-2017"
-#
-# #tutorial
-# #https://lyzander.github.io/tableHTML/
-#
-# ##########################################
-# ##########    Outras opções   ############
-# ##########    para tabelas    ############
-# ##########################################
-#
-# #http://stackoverflow.com/questions/3438226/exporting-r-tables-to-html
-#
-# ##########################################
-# ############    tableHTML     ############
-# ##########################################
-#
-# #vignette do tableHTML
-# #https://lyzander.github.io/tableHTML/
-#
-# #formatted tables with superscript
-# dados <- mtcars
-# dados$mpg[1] <- paste0(dados$mpg[1],'<sup>ABCd</sup>')
-# tab1 <- tableHTML(dados, widths = c(140, rep(50, 11)), second_header = list(c(3, 4, 5), c('col1', 'col2', 'col3')), theme = 'rshiny-blue',footer = 'Número superscrito é exemplo de teste de significancia')
-# tab2 <- tableHTML(dados, widths = c(140, rep(50, 11)), second_header = list(c(3, 4, 5), c('col1', 'col2', 'col3')), theme = 'scientific',footer = 'Número superscrito é exemplo de teste de significancia')
-# tab3 <- tableHTML(dados, widths = c(140, rep(50, 11)), second_header = list(c(3, 4, 5), c('col1', 'col2', 'col3')), theme = 'default',footer = 'Número superscrito é exemplo de teste de significancia')
-# write_tableHTML(tab1, file = paste0(dir,'\\tab.html'))
-# write_tableHTML(tab1, file = paste0(dir,'\\tab1.html'))
-# write_tableHTML(tab2, file = paste0(dir,'\\tab2.html'))
-# write_tableHTML(tab3, file = paste0(dir,'\\tab3.html'))
-#
-#
-# ##########################################
-# ##############    EXCEL     ##############
-# ##########################################
-#
-# require(RDCOMClient)
-#
-# xl <- COMCreate("Excel.Application")
-# xl[["Visible"]] <- TRUE
-# xl[["DisplayAlerts"]] <- FALSE
-# #wb <- xl[["Workbooks"]]$Add()
-# #sheet <- wb$Worksheets(1)
-#
-# file.xlsx <- "E:\\DADOS\\CONSULTORIA\\PESSOAS JURÍDICAS\\FlyFrog\\Perception - 10-04-2017\\processamento.xlsx"
-# file <- "E:\\DADOS\\CONSULTORIA\\PESSOAS JURÍDICAS\\FlyFrog\\Perception - 10-04-2017\\tab.html"
-# file1 <- "E:\\DADOS\\CONSULTORIA\\PESSOAS JURÍDICAS\\FlyFrog\\Perception - 10-04-2017\\tab1.html"
-# file2 <- "E:\\DADOS\\CONSULTORIA\\PESSOAS JURÍDICAS\\FlyFrog\\Perception - 10-04-2017\\tab2.html"
-# file3 <- "E:\\DADOS\\CONSULTORIA\\PESSOAS JURÍDICAS\\FlyFrog\\Perception - 10-04-2017\\tab3.html"
-# files <- c(file1,file2,file3)
-#
-# df.names <- data.frame(sheet="Initial")
-#
-# wb = xl$Workbooks()$Open(file)
-# sheet <- wb$Sheets(1)
-# sheet[["Name"]] <- "Initial"
-# #new <- wb$Worksheets()$Add()
-#
-# cont <- 0
-# for (i in files){
-#
-#   cont <- cont + 1
-#
-#   #inport html table
-#   html = xl$Workbooks()$Open(i)
-#
-#   #nome plan
-#   nome <- paste0("tabela-",cont)
-#   df.names <- df.names %>% add_row(sheet=nome)
-#
-#   #rename sheet
-#   html_sheet <- html$Sheets(1)
-#   html_sheet[["Name"]] <- nome
-#
-#   #move sheet to xlsx main file
-#   html$Sheets(1)$Move(After=sheet)
-#
-#   #remove html file
-#   file.remove(i)
-#
-# }
-#
-# #############################################
-# #add hyper-links to tabs
-#
-# #ActiveSheet.Hyperlinks.Add Anchor:=Selection, Address:="", SubAddress:= "'tabela-1'!A1", TextToDisplay:="tab1"
-#
-# df.names$nome <- paste0('Link para ',df.names$sheet)
-# df.names$adress <- paste0("'",df.names$sheet,"'!A1")
-#
-# #new <- wb$Worksheets()$Add(Before=sheet)
-# #new[["Name"]] <- "Índice"
-# tab_indice <- tableHTML(df.names[,-3],theme = 'rshiny-blue',rownames = FALSE)
-# write_tableHTML(tab_indice, file = paste0(dir,'\\tab_indice.html'))
-# html = xl$Workbooks()$Open(paste0(dir,'\\tab_indice.html'))
-# html_sheet <- html$Sheets(1)
-# html_sheet[["Name"]] <- "Índice"
-# html$Sheets(1)$Move(Before=sheet)
-# indice <- wb$sheets("Índice")
-# indice$Activate()
-#
-# for (i in 1:dim(df.names)[1]){
-#   local <- indice$Cells(i+1,2)
-#   indice$Hyperlinks()$Add(Anchor=local,Address="",SubAddress= df.names$adress[i], TextToDisplay=df.names$nome[i])
-# }
-#
-# wb$SaveAs(Filename=file.xlsx,FileFormat=51)
-# wb$Close()
-# xl$Quit()
+xlsx_table <- function(df,wb,sheet,tit.sep = NA,lin.inic = 1,col.inic = 1,width.first = 30,width.cols = 20){
+  #' Generate nice formated tables in excel.
+  #'
+  #' This function exports a dataframe to a formated table in excel. Format parameters are still hard-coded
+  #' in function. Cells of the table title will be merged if needed.
+  #'
+  #' @param df The \emph{dataframe} to be exported.
+  #' @param wb A \emph{workbook} object (created by \code{\link[openxlsx]{createWorkbook}}), identifying where the table will be created.
+  #' @param sheet A \emph{intenger} identifying in which sheet index to create the table.
+  #' @param tit.sep [Optional] A \emph{regular expression} identifying how variable names and values are separated in
+  #' the dataframe title. For example, in "variable_category" the tit.sep parameter would be "_".
+  #' variables are crossed by (usually reagion). The target variables will match the population within each label of
+  #' the crossing variable. These variables should exist in both the sample and population dataframes.
+  #' @param lin.inic [Optional] A \emph{integer} identifying in which line to insert the table.
+  #' @param col.inic [Optional] A \emph{integer} identifying in which column to insert the table.
+  #' @param width.first [Optional] A \emph{integer} identifying the width of the first column in the table.
+  #' @param width.cols [Optional] A \emph{integer} identifying  the width of the other columns in the table.
+  #' @return \emph{NULL}: the table is written to the provided workbook, no object is returned.
+  #' @examples
+  #'
+  #'### carregando dados
+  #'data(svy)
+  #'
+  #'### formatando dados
+  #'df = svy
+  #'df$pop <- runif(nrow(df))
+  #'df <- df %>% gather(var,categ,ends_with('cota'))
+  #'df <- df %>% group_by(regiao,var,categ) %>% summarise(pop=sum(pop))
+  #'df <- df %>% unite(var,var,categ,sep = "#")
+  #'df1 <- df
+  #'df <- df %>% spread(var,pop)
+  #'
+  #'### gerando tabelas
+  #'file <- "C:\\tabela_xlsx.xlsx"
+  #'wb <- createWorkbook()
+  #'addWorksheet(wb, "Exemplos")
+  #'# Tabela com 1 linha no título
+  #'xlsx_table(df1,wb=wb,sheet=1,lin.inic = 1,col.inic = 1)
+  #'# Tabela com 2 linhas no título
+  #'xlsx_table(df,wb=wb,sheet=1,tit.sep = "#",lin.inic = 1,col.inic = 5)
+  #'saveWorkbook(wb, file, overwrite = TRUE)
+
+  ### estilos
+
+  style_title_top <- createStyle(fontColour='blue1',halign = 'center', valign = 'center', textDecoration = 'bold',
+                                 fgFill="lightcyan",wrapText = TRUE,border=c('top','bottom','left','right'),
+                                 borderStyle = c('medium','medium','medium','medium'))
+  style_title_bot <- createStyle(fontColour='blue1',halign = 'center', valign = 'center', textDecoration = 'bold',
+                                 fgFill="lightcyan",wrapText = TRUE,border=c('top','bottom','left','right'),
+                                 borderStyle = c('medium','double','medium','medium'))
+  style_fisrt_column <- createStyle(fontColour='black',halign = 'left', valign = 'center', textDecoration = 'bold',
+                                    fgFill="lightgrey",wrapText = TRUE, border=c('right'), borderStyle = c('thin'))
+  style_last_column <- createStyle(fontColour='black',halign = 'left', valign = 'center', textDecoration = NULL,
+                                   wrapText = TRUE, border=c('left'), borderStyle = c('thin'))
+  style_data <- createStyle(fontColour='black',halign = 'center', valign = 'center', textDecoration = NULL,
+                            wrapText = FALSE)
+  style_last_row <- createStyle(fontColour='black',halign = 'center', valign = 'center', textDecoration = NULL,
+                                wrapText = FALSE,border=c('top'), borderStyle = c('thin'))
+
+  ### função
+
+  col.fim <- col.inic + ncol(df) - 1
+  cols <- length(col.inic:col.fim)
+
+  titulo <- names(df)
+  if (length(tit.sep) > 0){
+    top <- str_extract(titulo,paste0("^.*(?=",tit.sep,")"))
+    bot <- str_extract(titulo,paste0("(?<=",tit.sep,").*$"))
+    merge <- is.na(bot)
+  } else {
+    merge <- rep(TRUE,length(titulo))
+  }
+  if (sum(merge) == length(merge)){
+    writeData(wb, sheet = sheet,df,startRow = lin.inic,startCol = col.inic, colNames = TRUE,rowNames = FALSE)
+    addStyle(wb, sheet=sheet,style_title_bot, rows=lin.inic, cols=col.inic:col.fim,gridExpand = TRUE)
+
+    lin.fim <- lin.inic + nrow(df)
+    lin.tit <- lin.inic
+  } else {
+    top[merge] <- titulo[merge]
+    names(df) <- bot
+
+    writeData(wb, sheet = sheet,as.data.frame(t(top)),startRow =lin.inic,startCol = col.inic,colNames = FALSE,rowNames = FALSE)
+    writeData(wb, sheet = sheet,df,startRow = lin.inic+1,startCol = col.inic,colNames = TRUE,rowNames = FALSE)
+
+    #merging top 2 rows
+    pos.merge <- which(merge == TRUE) + col.inic - 1
+    walk(pos.merge,~mergeCells(wb, sheet, cols = ., rows = lin.inic:(lin.inic+1)))
+    #merging columns from same variable
+    runs <- rle(top)
+    if (sum(runs$lengths > 1) > 0){
+      len <- runs$lengths[runs$lengths > 1]
+      fim <- cumsum(runs$lengths)[runs$lengths > 1] + col.inic - 1
+      inic <- fim - len + 1
+      label <- runs$values[runs$lengths > 1]
+      walk2(inic,fim,~mergeCells(wb, sheet, cols =.x:.y, rows = lin.inic))
+    }
+
+    addStyle(wb, sheet=sheet,style_title_top, rows=lin.inic, cols=col.inic:col.fim,gridExpand = TRUE)
+    addStyle(wb, sheet=sheet,style_title_bot, rows=lin.inic+1, cols=col.inic:col.fim,gridExpand = TRUE)
+
+    lin.fim <- lin.inic + nrow(df) + 1
+    lin.tit <- lin.inic + 1
+  }
+
+  setColWidths(wb, sheet, cols = col.inic:col.fim, widths = c(width.first,rep(width.cols,cols-1)))
+
+  addStyle(wb, sheet=sheet,style_fisrt_column, rows=(lin.tit+1):lin.fim, cols=col.inic,gridExpand = TRUE)
+  addStyle(wb, sheet=sheet,style_data, rows=(lin.tit+1):lin.fim, cols=(col.inic+1):col.fim,gridExpand = TRUE)
+  addStyle(wb, sheet=sheet,style_last_row, rows=lin.fim+1, cols=col.inic:col.fim,gridExpand = TRUE)
+  addStyle(wb, sheet=sheet,style_last_column, rows=lin.inic:lin.fim, cols=col.fim+1,gridExpand = TRUE)
+
+}
+
