a17c4e502cdb09a5bf25b75b01c86c18e3a3498d
NEALE AHMED EL-DASH <neale.eldash@gmail.com>
2019-05-06 15:48:20 -0300

    fixing check raking functions - addind ,na.rm = TRUE

--- R/sampling_raking.R
+++ R/sampling_raking.R
@@ -518,7 +518,7 @@ rake_df <- function(df.svy=NA,df.pop=NA,reg.exp.vars=NA,reg.exp.cruz=NA,reg.exp.
   ####################################
 
   check <- df.svy[,c('weights','cruz',wgts)]
-  check <- check %>% tidyr::gather(var,categ,-weights,-cruz) %>% dplyr::group_by(cruz,var,categ) %>% dplyr::summarise(sample=n(),raw=n(),max.wgt = max(weights),min.wgt = min(weights),weights=sum(weights))
+  check <- check %>% tidyr::gather(var,categ,-weights,-cruz) %>% dplyr::group_by(cruz,var,categ) %>% dplyr::summarise(sample=n(),raw=n(),max.wgt = max(weights,na.rm = TRUE),min.wgt = min(weights,na.rm = TRUE),weights=sum(weights,na.rm = TRUE))
   check <- dplyr::full_join(check,targets,by=c('cruz','var','categ'))
   var.tot <- check$var[1]
   check.tot <- check %>% ungroup() %>% dplyr::filter(var == var.tot)
@@ -529,7 +529,7 @@ rake_df <- function(df.svy=NA,df.pop=NA,reg.exp.vars=NA,reg.exp.cruz=NA,reg.exp.
     diff=weights - pop
   )
 
-  check.tot <- check.tot %>% dplyr::group_by(cruz) %>% dplyr::summarise_at(vars(-cruz,-var,-categ,-ends_with('.wgt')),funs(sum(.)))
+  check.tot <- check.tot %>% dplyr::group_by(cruz) %>% dplyr::summarise_at(vars(-cruz,-var,-categ,-ends_with('.wgt')),funs(sum(.,na.rm = TRUE)))
   check.tot <- check.tot %>% ungroup() %>% dplyr::mutate(
     raw=round(100*raw/sum(raw,na.rm = TRUE),1),
     weights=round(100*weights/sum(weights,na.rm = TRUE),1),
@@ -727,7 +727,7 @@ rake_target <- function(df.svy=NA,targets=NA,reg.exp.vars=NA,reg.exp.cruz=NA,reg
   ####################################
 
   check <- df.svy[,c('weights','cruz',wgts)]
-  check <- check %>% tidyr::gather(var,categ,-weights,-cruz) %>% dplyr::group_by(cruz,var,categ) %>% dplyr::summarise(sample=n(),raw=n(),max.wgt = max(weights),min.wgt = min(weights),weights=sum(weights))
+  check <- check %>% tidyr::gather(var,categ,-weights,-cruz) %>% dplyr::group_by(cruz,var,categ) %>% dplyr::summarise(sample=n(),raw=n(),max.wgt = max(weights,na.rm = TRUE),min.wgt = min(weights,na.rm = TRUE),weights=sum(weights,na.rm = TRUE))
   check <- dplyr::full_join(check,targets,by=c('cruz','var','categ'))
   var.tot <- check$var[1]
   check.tot <- check %>% ungroup() %>% dplyr::filter(var == var.tot)
@@ -738,7 +738,7 @@ rake_target <- function(df.svy=NA,targets=NA,reg.exp.vars=NA,reg.exp.cruz=NA,reg
     diff=weights - pop
   )
 
-  check.tot <- check.tot %>% dplyr::group_by(cruz) %>% dplyr::summarise_at(vars(-cruz,-var,-categ,-ends_with('.wgt')),funs(sum(.)))
+  check.tot <- check.tot %>% dplyr::group_by(cruz) %>% dplyr::summarise_at(vars(-cruz,-var,-categ,-ends_with('.wgt')),funs(sum(.,na.rm = TRUE)))
   check.tot <- check.tot %>% ungroup() %>% dplyr::mutate(
     raw=round(100*raw/sum(raw,na.rm = TRUE),1),
     weights=round(100*weights/sum(weights,na.rm = TRUE),1),
